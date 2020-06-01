5917ecb3cd35965ea347ed54ec528a557e64a715
jakep5 <jakepagel1@yahoo.com>
2020-06-01 11:38:53 -0500

    configure cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); 
+  res.header("Access-Control-Allow-Origin", "https://time-tracker-seven.now.sh/"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
